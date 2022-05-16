2e251398734e6d460a4d15bd9ba3ed7370178415
Krzysiek <krzysztofkulpacb@gmail.com>
2022-05-16 21:56:38 +0200

    first commit

---
+++ src/test/java/com/pokemon/pokemongame/PokemonGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.pokemongame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
