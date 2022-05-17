0f9079d75d07aceea063f0d88f45385f28b7f9d0
Krzysiek <krzysztofkulpacb@gmail.com>
2022-05-17 22:25:09 +0200

    0.0.0.3 GSON dependency added

--- src/main/java/com/pokemon/pokemongame/webclient/pokemon/PokemonClient.java
+++ src/main/java/com/pokemon/pokemongame/webclient/pokemon/PokemonClient.java
@@ -2,14 +2,20 @@ package com.pokemon.pokemongame.webclient.pokemon;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.pokemon.pokemongame.model.MoveDto;
 import com.pokemon.pokemongame.model.PokemonDto;
 import com.pokemon.pokemongame.webclient.pokemon.dto.OpenMoveDto;
 import com.pokemon.pokemongame.webclient.pokemon.dto.OpenMovesDto;
 import com.pokemon.pokemongame.webclient.pokemon.dto.OpenPokemonDto;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,17 +28,18 @@ public class PokemonClient {
     public PokemonDto getPokemon(String id){
         String url = API_URL + id;
         String json = restTemplate.getForObject(url, String.class);
-
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         try{
+
             OpenPokemonDto response = objectMapper.readValue(json, OpenPokemonDto.class);
 
             List<MoveDto> moves = response.getMoves().stream()
                     .map(OpenMovesDto::getMove)
                     .map(move -> new MoveDto(move.getName(), move.getUrl()))
                     .collect(Collectors.toList());
+
 
             return PokemonDto.builder()
                     .id(response.getId())
