2c5f7013a17c212af9dd4904dab9d14a2cb2e484
Krzysiek <krzysztofkulpacb@gmail.com>
2022-05-23 14:21:53 +0200

    0.0.0.7 pokedex refactored

--- src/main/java/com/pokemon/pokemongame/gui/profile/Pokedex.java
+++ src/main/java/com/pokemon/pokemongame/gui/profile/Pokedex.java
@@ -4,6 +4,7 @@ package com.pokemon.pokemongame.gui.profile;
 import com.pokemon.pokemongame.gui.MainLayout;
 import com.pokemon.pokemongame.weblogic.model.PokemonDto;
 import com.pokemon.pokemongame.weblogic.service.PokemonService;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.formlayout.FormLayout;
@@ -27,53 +28,77 @@ public class Pokedex extends HorizontalLayout {
     @Autowired
     private PokemonService pokemonService;
 
+    private Image imageFront;
+    private Image imageBack;
+    private TextField pokemonId;
+    private TextField pokemonName;
+    private TextField pokemonWeight;
+    private TextField pokemonHeight;
+    private FormLayout formLayout;
+    private TextField idInputTextField;
+    private Button getPokemonButton;
+
     public Pokedex() {
 
-        Image imageFront = new Image("https://www.shareicon.net/data/128x128/2016/08/01/640216_go_512x512.png", "pokeball");
-        Image imageBack = new Image("https://www.shareicon.net/data/128x128/2016/08/01/640216_go_512x512.png", "pokeball");
-        TextField pokemonId = new TextField("Pokemon ID", "ID");
+        initializeFormLayout();
+
+        initializeUserInputComponents();
+
+        add( formLayout, idInputTextField, getPokemonButton);
+    }
+
+    private void initializeFormLayout() {
+        imageFront = new Image("https://www.shareicon.net/data/128x128/2016/08/01/640216_go_512x512.png", "pokeball");
+        imageBack = new Image("https://www.shareicon.net/data/128x128/2016/08/01/640216_go_512x512.png", "pokeball");
+        pokemonId = new TextField("Pokemon ID", "ID");
         pokemonId.setReadOnly(true);
-        TextField pokemonName = new TextField("Pokemon name", "Name");
+        pokemonName = new TextField("Pokemon name", "Name");
         pokemonName.setReadOnly(true);
-        TextField pokemonWeight = new TextField("Pokemon weight", "Weight");
+        pokemonWeight = new TextField("Pokemon weight", "Weight");
         pokemonWeight.setReadOnly(true);
-        TextField pokemonHeight = new TextField("Pokemon height", "Height");
+        pokemonHeight = new TextField("Pokemon height", "Height");
         pokemonHeight.setReadOnly(true);
 
-        FormLayout formLayout = new FormLayout();
+        formLayout = new FormLayout();
+
         formLayout.add(imageFront, imageBack,
                 pokemonId, pokemonName,
                 pokemonWeight, pokemonHeight);
+
         formLayout.setResponsiveSteps(
                 new ResponsiveStep("0", 1),
                 new ResponsiveStep("100px", 2)
         );
+    }
 
-        TextField idInpuTextField = new TextField("Pokemon ID");
-        Button getPokemonButton = new Button("Find Pokemon");
-        getPokemonButton.addClickListener(e -> {
-            try {
-                String id = idInpuTextField.getValue();
-                PokemonDto pokemon = pokemonService.getPokemon(id);
+    private void initializeUserInputComponents() {
+        idInputTextField = new TextField("Pokemon ID");
+        getPokemonButton = new Button("Find Pokemon");
 
-                imageFront.setSrc(pokemon.getSpriteFront());
-                imageBack.setSrc(pokemon.getSpriteBack());
-                pokemonId.setValue(Integer.toString(pokemon.getId()));
-                pokemonName.setValue(pokemon.getName());
-                pokemonWeight.setValue(Integer.toString(pokemon.getWeight()));
-                pokemonHeight.setValue(Integer.toString(pokemon.getHeight()));
-
-
-            }catch (Exception exception){
-                Notification.show("Invalid ID number!");
-            }
-
-        });
+        getPokemonButton.addClickListener(e -> executeGetPokemonButton() );
+        getPokemonButton.addClickShortcut(Key.ENTER);
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, idInpuTextField, getPokemonButton);
+        setVerticalComponentAlignment(Alignment.END, idInputTextField, getPokemonButton);
 
-        add( formLayout, idInpuTextField, getPokemonButton);
+    }
+
+    private void executeGetPokemonButton() {
+        try {
+            String id = idInputTextField.getValue();
+            PokemonDto pokemon = pokemonService.getPokemon(id);
+
+            imageFront.setSrc(pokemon.getSpriteFront());
+            imageBack.setSrc(pokemon.getSpriteBack());
+            pokemonId.setValue(Integer.toString(pokemon.getId()));
+            pokemonName.setValue(pokemon.getName());
+            pokemonWeight.setValue(Integer.toString(pokemon.getWeight()));
+            pokemonHeight.setValue(Integer.toString(pokemon.getHeight()));
+
+
+        }catch (Exception exception){
+            Notification.show("Invalid ID number!");
+        }
     }
 
 }
